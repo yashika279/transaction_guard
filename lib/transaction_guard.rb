45fa3725bc1da59ac3c158226c1d13de3ad82eb9
Yashika <yashikavijay2799@gmail.com>
2026-09-11 13:46:29 +0530

    Initialize transaction_guard gem

---
+++ lib/transaction_guard.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "transaction_guard/version"
+
+module TransactionGuard
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/transaction_guard/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module TransactionGuard
+  VERSION = "0.1.0"
+end
